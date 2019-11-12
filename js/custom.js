68a7409dacdfde5b7335236c64f07999c0a96f25
NastyaZagulyayeva <nastyazagyl2@gmail.com>
2019-11-12 13:18:36 +0200

    first-comit

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+let myNav = document.getElementById('nav-bar');
+window.onscroll = function () { 
+    "use strict";
+    if (document.scrollingElement.scrollTop >= 759 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
+
